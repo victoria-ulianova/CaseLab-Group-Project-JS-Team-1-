9f7be99e5d4055dfe8e18c3aa194c1993ee22cd5
victoria-ulianova <victoria.ulianova@gmail.com>
2022-03-03 23:46:24 +0300

    Global Changes

---
+++ src/Components/Cart/CartTS/CartTS.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+interface ITodoProps{
+    title: string
+    checked: boolean
+    date?: string
+}
+
+export default class Todo extends Component<ITodoProps, any> {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
